45b7a7277da24f6e015ac35ede2908cf71a98801
Matteo Temporini <temporini.matteo@gmail.com>
2015-03-29 17:00:38 +0200

    Upload version 1.0

---
+++ remote_update_packages.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+# Script aggiornamento globale server Linux
+# V. 1.0
+
+# Server Debian / Ubuntu
+
+server_apt=(
+        ns1.domain.tld
+        ns2.domain.tld
+        srvcld01.domain.tld
+)
+
+server_yum=(
+        ns3.domain.tld
+)
+
+for server in ${server_apt[@]}
+ do
+        echo $server
+        ssh root@$server 'apt-get update && apt-get -y upgrade'
+ done
+
+for server in ${server_yum[@]}
+ do
+        echo $server
+        ssh root@$server 'yum update'
+ done
